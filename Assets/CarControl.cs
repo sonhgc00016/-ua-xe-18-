e2bfd699c8a525767c1d5c84631905f45b76d9e8
SONHGC00016 <sonhgc00016@gmail.com>
2014-02-07 18:21:30 +0700

    SONHGC00016

---
+++ Assets/CarControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarControl : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Camera.main.transform.position.z >= (this.transform.position.z + 20)) {
+						Vector3 v = this.transform.position;
+						v.x = 0;
+						v += new Vector3 (Random.Range (-4.5f, 4.5f), 0, Random.Range (40, 60));
+						this.transform.position = v;
+				}
+		}
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+		public float moveSpeed = 0.5f;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				this.transform.position += new Vector3 (Input.GetAxis ("Mouse X"), 0, moveSpeed);
+				Camera.main.transform.position += new Vector3 (0, 0, moveSpeed);
+		}
+
+		void OnTriggerEnter (Collider other)
+		{
+				if (other.name == "Car") {
+						Debug.Log ("A' Dau qua!");
+				}
+		}
+}
